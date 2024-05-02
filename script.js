9aa98bb34a9a35a18c044eff398c3c5f293547b1
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-05-02 09:19:01 -0600

    Adds getHumanChoice function

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 console.log("Hello World");
 getComputerChoice();
+getHumanChoice();
 
 function getComputerChoice() {
     const compChoiceInt = Math.floor(Math.random() * 3);
     console.log(compChoiceInt);
     
     if (compChoiceInt == 0) {
-        return "Rock";
+        var compChoice = "Rock"
+        console.log(compChoice);
+        return compChoice;
     } else if (compChoiceInt == 1) {
-        return "Paper";
+        var compChoice = "Rock"
+        console.log(compChoice);
+        return compChoice;
     } else if (compChoiceInt == 2) {
-        return "Scissor";
+        var compChoice = "Rock"
+        console.log(compChoice);
+        return compChoice;
     } else {
         console.log("Error");
     }
-    
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+    console.log(humanChoice);
 }
