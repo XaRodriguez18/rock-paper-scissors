03c1408f97842bb96e0c3983c37ab20262eeba61
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-05-02 09:41:55 -0600

    Adds playRound function

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+/* Step 1 */
 console.log("Hello World");
 getComputerChoice();
 getHumanChoice();
 
+/* Step 2 */
 function getComputerChoice() {
     const compChoiceInt = Math.floor(Math.random() * 3);
     console.log(compChoiceInt);
     
     if (compChoiceInt == 0) {
-        var compChoice = "Rock"
+        var compChoice = "rock"
         console.log(compChoice);
         return compChoice;
     } else if (compChoiceInt == 1) {
-        var compChoice = "Rock"
+        var compChoice = "paper"
         console.log(compChoice);
         return compChoice;
     } else if (compChoiceInt == 2) {
-        var compChoice = "Rock"
+        var compChoice = "scissors"
         console.log(compChoice);
         return compChoice;
     } else {
@@ -23,7 +25,42 @@ function getComputerChoice() {
     }
 }
 
+/* Step 3 */
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper or Scissors?");
-    console.log(humanChoice);
+    let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
+    if (humanChoice.toLowerCase() === "rock" || humanChoice.toLowerCase() === "paper" || humanChoice.toLowerCase() === "scissors") {
+        console.log(humanChoice);
+        return humanChoice;
+    } else {
+        alert("Please choose a correct option.")
+    }
+}
+
+/* Step 4 */
+var humanScore = 0;
+var computerScore = 0;
+
+/* Step 5 */
+function playRound(humanChoice, compChoice) {
+ let alerMessage = "";
+ let humanWon = undefined;
+
+ if (humanChoice === compChoice) {
+    humanWon = undefined;
+ } else if (
+    (humanChoice === "rock" && compChoice === "paper") ||
+    (humanChoice === "paper" && compChoice === "scissors") ||
+    (humanChoice === "scissors" && compChoice === "rock")
+ ) {
+    humanWon = false;
+ } else if (
+    (humanChoice === "rock" && compChoice === "scissors") ||
+    (humanChoice === "paper" && compChoice === "rock") ||
+    (humanChoice === "scissors" && compChoice === "paper")
+ ) {
+    humanWon = true;
+ }
+
+ return humanWon;
+ 
 }
