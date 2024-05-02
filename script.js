c50f0f9264f43c58f685b7aa0f9b5e85dbde2e18
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-05-02 10:02:54 -0600

    Placed funtions and varibales into playGame

--- script.js
+++ script.js
@@ -30,35 +30,53 @@ function getHumanChoice() {
 }
 
 /* Step 4 */
-var humanScore = 0;
-var computerScore = 0;
+
 
 /* Step 5 */
-function playRound(humanChoice, compChoice) {
- let alerMessage = "";
- let humanWon = undefined;
 
- if (humanChoice === compChoice) {
-    humanWon = undefined;
- } else if (
-    (humanChoice === "rock" && compChoice === "paper") ||
-    (humanChoice === "paper" && compChoice === "scissors") ||
-    (humanChoice === "scissors" && compChoice === "rock")
- ) {
-    humanWon = false;
- } else if (
-    (humanChoice === "rock" && compChoice === "scissors") ||
-    (humanChoice === "paper" && compChoice === "rock") ||
-    (humanChoice === "scissors" && compChoice === "paper")
- ) {
-    humanWon = true;
- }
 
- return humanWon;
 
-}
-
-const humanSelection =  getHumanChoice();
-const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+function playGame() {
+    var humanScore = 0;
+    var computerScore = 0;
+
+    const humanSelection =  getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    function playRound(humanChoice, compChoice) {
+        let alertMessage = "";
+        let humanWon = undefined;
+       
+        if (humanChoice === compChoice) {
+           humanWon = undefined;
+        } else if (
+           (humanChoice === "rock" && compChoice === "paper") ||
+           (humanChoice === "paper" && compChoice === "scissors") ||
+           (humanChoice === "scissors" && compChoice === "rock")
+        ) {
+           humanWon = false;
+        } else if (
+           (humanChoice === "rock" && compChoice === "scissors") ||
+           (humanChoice === "paper" && compChoice === "rock") ||
+           (humanChoice === "scissors" && compChoice === "paper")
+        ) {
+           humanWon = true;
+        }
+       
+        
+       
+        if (humanWon === undefined) {
+           alertMessage = 'Draw \n\nScore:\nYou: ${humanScore} | Computer ${computerscore}';
+        } else if (humanWon === true) {
+           humanScore += 1;
+           alertMessage = "You win! ${humanChoice} beats ${compChoice}. \n\nScore:\nYou: ${humanScore} | Computer ${computerscore}";
+        } else if (humanWon === false) {
+           computerScore += 1;
+           alertMessage = "You lose! ${compChoice} beats ${humanChoice} \n\nScore:\nYou: ${humanScore} | Computer ${computerscore}";
+        }
+       
+    }
+}
