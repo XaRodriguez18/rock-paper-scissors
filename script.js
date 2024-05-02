af2b6136d7362433626679663e83b736bbfee9be
XaRod18 <xander.rodriguez.2006@gmail.com>
2024-05-02 09:10:01 -0600

    Assigns random number a string value

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ console.log("Hello World");
 getComputerChoice();
 
 function getComputerChoice() {
-    let compChoiceInt = Math.floor(Math.random() * 3);
+    const compChoiceInt = Math.floor(Math.random() * 3);
     console.log(compChoiceInt);
+    
+    if (compChoiceInt == 0) {
+        return "Rock";
+    } else if (compChoiceInt == 1) {
+        return "Paper";
+    } else if (compChoiceInt == 2) {
+        return "Scissor";
+    } else {
+        console.log("Error");
+    }
 }
